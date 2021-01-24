eef8f740709d4ed9c0a0e1d5072e525a4ebc4cfe
Michał Sołtysiak <slsm@viessmann.com>
2021-01-24 13:44:00 +0100

    probin margin extended to avoid cariage colision with the frame when drag chain added

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -999,7 +999,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 10
+#define PROBING_MARGIN 25
 
 
 // X and Y axis travel speed (mm/min) between probes
@@ -1452,7 +1452,7 @@
 #define LEVEL_BED_CORNERS
 
 #if ENABLED(LEVEL_BED_CORNERS)
-  #define LEVEL_CORNERS_INSET_LFRB { 45, 23, 45, 23 } // (mm) Left, Front, Right, Back insets
+  #define LEVEL_CORNERS_INSET_LFRB { 45, 50, 45, 50 } // (mm) Left, Front, Right, Back insets
   #define LEVEL_CORNERS_HEIGHT      0.1   // (mm) Z height of nozzle at leveling points
   #define LEVEL_CORNERS_Z_HOP       4.0   // (mm) Z height of nozzle between leveling points
   //#define LEVEL_CENTER_TOO              // Move to the center after the last corner
