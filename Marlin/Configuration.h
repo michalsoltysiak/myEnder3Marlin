505210cdd09d2aa62d812961e9d8b6e56b3b9756
Michał Sołtysiak <slsm@viessmann.com>
2021-02-27 14:57:33 +0100

    boot logo changed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -76,7 +76,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(MichalMS, Ender-3 Pro)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Michał Sołtysiak, Ender-3 Pro)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
